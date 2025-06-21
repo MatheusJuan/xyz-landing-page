edb9502cb1af331bcb853bc9799deecc96391344
MatheusJuan <contatodantex@gmail.com>
2025-06-21 12:54:10 -0300

    feat: add microsoft clarity tracking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,18 @@ export default defineNuxtConfig({
     head: {
       link: [
         { rel: 'icon', type: 'image/png', href: '/images/favicon.ico' }
+      ],
+      script: [
+        {
+          innerHTML: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "s30zp4sluy");
+          `,
+          type: 'text/javascript'
+        }
       ]
     }
   }
